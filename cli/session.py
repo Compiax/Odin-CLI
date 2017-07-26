8de617ec4cf17b8b790d60bbd35438520bfa3d2d
Jason van Hattum <jasonvhattum@gmail.com>
2017-07-26 10:58:36 +0200

    Added socket communication

--- cli/session.py
+++ cli/session.py
@@ -5,6 +5,8 @@
 #   Created by Jason van Hattum - 23/07/2017
 
 from Variable_Handler.variableHandler import *
+import socket
+from printColors import *
 
 class Session:
     """
@@ -15,13 +17,64 @@ class Session:
     def __init__(self):
         self.variables = VariableHandler()
         self.operations = []
-        self.components =[]
+        self.components = []
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if self.connectToDaemon():
+            print("{}connected{}".format(PrintColors.OKBLUE, PrintColors.ENDC))
+            self.connected = True
+        else:
+            print("{}unable to connect - running in offline mode{}".format(PrintColors.FAIL, PrintColors.ENDC))
+            self.connected = False
+    
+    def connectToDaemon(self):
+        connectionDetails = {'hostname': 'localhost', 'port':8000}
+        print("> Attemping to connect to Odin Daemon on {}:{} .. ".format(connectionDetails['hostname'], connectionDetails['port']),end='')
+        try:
+            self.socket.connect((connectionDetails['hostname'], connectionDetails['port']))
+        except ConnectionRefusedError:            
+            return False
+        return True
 
-    def execute_session(self):
+    def sendToDaemon(self, data):
+        amountToSend = len(data)
+        if (amountToSend == 0):
+            print("> {}Nothing to send to the Daemon - session is empty{}".format(PrintColors.FAIL, PrintColors.ENDC))
+            return
+        print("> Sending {} bytes to the Daemon.. ".format(amountToSend), end='')
+
+        totalsent = 0
+        while totalsent < amountToSend:
+            sent = self.socket.send(data[totalsent:])
+            if sent == 0:
+                print("{}Failed - Deamon connection broken{}".format(PrintColors.FAIL, PrintColors.ENDC))
+                self.connected = false
+                return
+            totalsent = totalsent + sent
+        print("{}{} variable(s) and {} operation(s) sent to Daemon.{}".format(
+            PrintColors.OKBLUE,
+            len(self.variables.list),
+            len(self.operations),
+            PrintColors.ENDC
+        ))
+
+    def execute(self):
         """
         Executes the session, sending all variables and operations to the Daemon
         """
-       
+        if (not self.connected):
+            if (self.connectToDaemon()):
+                print("{}connected{}".format(PrintColors.OKBLUE, PrintColors.ENDC))
+                self.connected = True
+            else:
+                print("{}unable to connect - cannot execute the session{}".format(PrintColors.FAIL, PrintColors.ENDC))
+                self.connected = False
+                return
+
+        toSend = bytearray()
+        toSend.extend(map(ord, self.toJson()))
+        # Do the sending
+        # self.connectToDaemon()
+        self.sendToDaemon(toSend)
 
     def print(self):
         print("Variables:")
@@ -30,3 +83,13 @@ class Session:
         print("Operations:")
         for op in self.operations:
             print("- {}".format(op))
+    
+    def toJson(self):
+        json = '\n'.join(map(lambda x: x.convertToJson(),self.variables.list))
+        if len(json) > 0:
+            json += '\n'  # Could also add some other separater between the variables and the commands
+        json += '\n'.join(map(str,self.operations))
+        return json
+
+
+    
--- cli/shell.py
+++ cli/shell.py
@@ -141,16 +141,28 @@ class OdinShell(Cmd):
             else:
                 print(PrintColors.FAIL + "ERROR: No such variable exists" + PrintColors.ENDC)
 
+    def do_execute(self, args):
+        """
+        Executes the session.
+        """
+        self.session.execute()
+
+    def do_do(self, args):
+        """
+        Performs an operation
+        """
+        Operations.validate_arguments_and_add(self.session,args)
+
     ##########################################################
     #   Write/reading files (export/import)
     ###########################################################
     def do_export(self, args):
         """Exports the current variables to a file."""
-        print ("'execute' called with arguments {}".format(repr(args)))
+        print ("'export' called with arguments {}".format(repr(args)))
 
     def do_import(self, args):
         """Imports the current variables from a file."""
-        print ("'execute' called with arguments {}".format(repr(args)))
+        print ("'import' called with arguments {}".format(repr(args)))
 
     ###########################################################
     #   Quit
